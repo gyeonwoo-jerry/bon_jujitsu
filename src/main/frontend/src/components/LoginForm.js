814352ebbf620fa10da37afa1f4314d518d1e5d8
goootae <goootae@gmail.com>
2025-03-20 13:05:31 +0900

    개발 환경에서 추가 수정

--- src/main/frontend/src/components/LoginForm.js
+++ src/main/frontend/src/components/LoginForm.js
@@ -10,7 +10,7 @@ function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("NODE_ENV:", process.env.NODE_ENV); // 출력된 값 확인
+    console.log("NODE_ENV11111:", process.env.NODE_ENV); // 출력된 값 확인
     console.log("Spring Profile:", process.env.REACT_APP_SPRING_PROFILE); // `REACT_APP_SPRING_PROFILE`으로 접근
     console.log("Spring REACT_APP_API_URL:", process.env.REACT_APP_API_URL); // `REACT_APP_SPRING_PROFILE`으로 접근
     // 로그인 처리 로직
