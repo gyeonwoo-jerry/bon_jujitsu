3481d2f753ea875a8f27ac7d75a839fa327b0440
goootae <goootae@gmail.com>
2025-03-20 11:58:18 +0900

    테스트 포트

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080"; // 기본 포트 설정
+  const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:38080"; // 기본 포트 설정
 
   app.use(
     "/api",
