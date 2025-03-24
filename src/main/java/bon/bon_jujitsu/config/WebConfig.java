40350673df90e0c95c6ef779e584c1cf22e12ed0
goootae <goootae@gmail.com>
2025-03-24 17:40:43 +0900

    빌드 그래들 관련 설정 수정

--- src/main/java/bon/bon_jujitsu/config/WebConfig.java
+++ src/main/java/bon/bon_jujitsu/config/WebConfig.java
@@ -2,8 +2,6 @@ package bon.bon_jujitsu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
