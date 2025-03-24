d3f2c5bd2089adade0bfeafb3a0b8b75286c633b
et03 <et03@ezylab.co.kr>
2025-03-24 17:45:33 +0900

    modi2

---
+++ src/main/frontend/src/components/BranchList.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import API from "../utils/api";
+import '../styles/branchList.css';
+
+function BranchList() {
+    const [branches, setBranches] = useState([]);
+
+    useEffect(() => {
+        // API를 통해 모든 branch 데이터를 가져옵니다.
+        API.get('/branch/all?page=1&size=10')
+            .then(response => {
+                if (response.status === 200) {
+                    setBranches(response.data.data);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching branch data:', error);
+            });
+    }, []);
+
+    return (
+        <div className="branchList_container">
+            <div className="inner">
+                            
+                <h1>Branch List</h1>
+                <ul>
+                    {branches.map(branch => (
+
+                        <li className='branch_item'  key={branch.id}>
+                            <div className='branch_item_title'>
+                                <div className='branch_item_title_tit'>
+                                    <div className='gym_name'>{branch.region}</div>
+                                    <div className='gym_area'>{branch.area}</div>
+                                </div>
+                                <button className='branch_item_title_more'>
+                                    <img src='/images/icon_click_wt.png' alt='더보기' />
+                                </button>
+                            </div>
+                            <div className='branch_info'>
+                                <div className='owner_name'>{branch.owner.name}</div>
+                                <div className='address'><font className='accent'>A.</font>{branch.address}</div>
+                                <div className='phone'><font className='accent'>T.</font>{branch.owner.phoneNum}</div>
+                                <div className='sns'>
+                                    <ul>
+                                        <li>
+                                            <a href={branch.owner.facebook} target='_blank' rel='noopener noreferrer'>
+                                                <img src='/images/icon-facebook.png' alt='페이스북' />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href={branch.owner.instagram} target='_blank' rel='noopener noreferrer'>
+                                                <img src='/images/icon-insta.png' alt='인스타그램' />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href={branch.owner.blog} target='_blank' rel='noopener noreferrer'>
+                                                <img src='/images/icon-blog.png' alt='블로그' />
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href={branch.owner.cafe} target='_blank' rel='noopener noreferrer'>
+                                                <img src='/images/icon-cafe.png' alt='카페' />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default BranchList;
