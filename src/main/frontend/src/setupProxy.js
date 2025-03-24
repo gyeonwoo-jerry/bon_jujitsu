eb2bd51c1158da40b588eb8e9ec1f2fd39f4504f
goootae <goootae@gmail.com>
2025-03-24 17:01:30 +0900

    클라이언트 라이팅 설정 추가

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  const apiUrl = process.env.REACT_APP_API_URL || "http://211.110.44.79:58080"; // 기본 포트 설정
+  const apiUrl = "http://211.110.44.79:58080"; // 기본 포트 설정
 
   app.use(
     "/api",
--- src/main/java/bon/bon_jujitsu/config/WebConfig.java
+++ src/main/java/bon/bon_jujitsu/config/WebConfig.java
@@ -2,6 +2,8 @@ package bon.bon_jujitsu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +15,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
 
