6ce15cecbe63608533c7da59716934da19c8ffaa
et03 <et03@ezylab.co.kr>
2025-03-26 16:39:48 +0900

    페이지 수정

--- src/main/frontend/src/pages/store.js
+++ src/main/frontend/src/pages/store.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SubHeader from '../components/SubHeader';
 
-function store() {
+function Store() {
+  const [pageName, setPageName] = useState('');
+  const [descName, setDescName] = useState('');
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    const title = '스토어'; 
+    setPageName(title);
+    document.title = title;
+    const descName = '환영합니다!';
+    setDescName(descName);
+    const backgroundImage = '/images/bon_academy_back@3x.png';
+    setBackgroundImage(backgroundImage);
+  }, []);
+
   return (
     <div className="store">
-      <h1>환영합니다!</h1>
-      <p>이것은 스토어입니다.</p>
+      <SubHeader pageName={pageName} descName={descName} backgroundImage={backgroundImage} />
+      <div className='store_container'>
+        <h1>환영합니다!</h1>
+        <p>이것은 스토어입니다.</p>
+      </div>
     </div>
   );
 }
 
-export default store; 
+export default Store; 
