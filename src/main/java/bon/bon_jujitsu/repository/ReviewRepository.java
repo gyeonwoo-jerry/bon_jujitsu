a656ec2e119bb0c111a2f5536813c312de1abd97
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-04-16 10:05:35 +0900

    [feat] 리뷰 레포지토리 수정

--- src/main/java/bon/bon_jujitsu/repository/ReviewRepository.java
+++ src/main/java/bon/bon_jujitsu/repository/ReviewRepository.java
@@ -4,7 +4,10 @@ import bon.bon_jujitsu.domain.Review;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository <Review, Long>{
 
@@ -12,6 +15,12 @@ public interface ReviewRepository extends JpaRepository <Review, Long>{
 
   List<Review> findAllByParentReviewIdIn(List<Long> parentReviewIds);
 
-  Page<Review> findAllByItem_IdOrderByCreatedAtDesc(Long itemId, PageRequest pageRequest);
+  @Query("SELECT r FROM Review r " +
+      "JOIN FETCH r.user " +
+      "LEFT JOIN FETCH r.order " +
+      "LEFT JOIN FETCH r.images " +
+      "WHERE r.item.id = :itemId AND r.isDeleted = false " +
+      "ORDER BY r.createdAt DESC")
+  Page<Review> findAllByItem_IdOrderByCreatedAtDesc(@Param("itemId") Long itemId, Pageable pageable);
 }
 
