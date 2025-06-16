8f5d1c33618d13c1eb214b7b18778f0253c68c9b
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-06-16 13:40:14 +0900

    [feat] CartItemDTO 옵션 정보 추가

--- src/main/java/bon/bon_jujitsu/dto/CartItemDto.java
+++ src/main/java/bon/bon_jujitsu/dto/CartItemDto.java
@@ -1,6 +1,7 @@
 package bon.bon_jujitsu.dto;
 
 import bon.bon_jujitsu.domain.CartItem;
+import bon.bon_jujitsu.dto.response.ItemOptionResponse;
 
 public record CartItemDto(
     Long id,
@@ -10,7 +11,8 @@ public record CartItemDto(
     int currentPrice,   // 현재 상품 가격 (변동 가능)
     int quantity,
     int totalPrice,
-    boolean isPriceChanged  // 가격 변동 여부
+    boolean isPriceChanged,
+    ItemOptionResponse itemOption// 가격 변동 여부,
 ) {
   public CartItemDto(CartItem cartItem) {
     this(
@@ -21,8 +23,8 @@ public record CartItemDto(
         cartItem.getItem().getPrice(),
         cartItem.getQuantity(),
         cartItem.getPrice() * cartItem.getQuantity(),
-        cartItem.getPrice() != cartItem.getItem().getPrice()
+        cartItem.getPrice() != cartItem.getItem().getPrice(),
+        cartItem.getItemOption() != null ? ItemOptionResponse.fromEntity(cartItem.getItemOption()) : null
     );
   }
-
 }
