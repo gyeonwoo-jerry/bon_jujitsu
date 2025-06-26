f1ea3295f0949222c3479101a83e7497bf0dd4ea
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-06-26 18:15:24 +0900

    [feat] 필터 이미지 경로 예외 수정

--- src/main/java/bon/bon_jujitsu/filter/AuthenticationFilter.java
+++ src/main/java/bon/bon_jujitsu/filter/AuthenticationFilter.java
@@ -78,7 +78,7 @@ public class AuthenticationFilter implements Filter {
             requestUri.startsWith("/mypage") ||
             requestUri.startsWith("/order") ||
             requestUri.startsWith("/storeWrite") ||
-            requestUri.startsWith("/uploads/images/") || // 이미지 업로드 경로 예외 처리
+            requestUri.startsWith("/data/uploads/") || // 이미지 업로드 경로 예외 처리
             requestUri.startsWith("/api/qna") ||
             (isDevelopment && "OPTIONS".equalsIgnoreCase(httpMethod)) || // OPTIONS 메서드 추가
             (requestUri.startsWith("/api/board") && (isDevelopment || "GET".equalsIgnoreCase(
